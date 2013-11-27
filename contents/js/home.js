8d8956e70dded89b4c00a5975d759ba6b0c54503
nason <michael@nason.us>
2013-11-26 18:31:26 -0800

    Async loading of homepage scripts

--- contents/js/home.js
+++ contents/js/home.js
@@ -1,16 +1,8 @@
 var site = require('./init.js');
-var domready = require('./components/domready');
 
-domready(function() {
-  // Smooth scroll animations
-  require('./vendor/smooth-scroll.js');
-
-  // Full Page Header
-  require('./full-header.js');
-
-  // Homepage effects
-  require('./homepage-effects.js');
-
-  // Contact Form
-  require('./contact-form.js');
-});
+head.load([
+  '/js/vendor/smooth-scroll.js',
+  '/js/full-header.js',
+  '/js/homepage-effects.js',
+  '/js/contact-form.js'
+]);
